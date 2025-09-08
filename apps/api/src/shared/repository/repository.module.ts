c445551360b751da58c364a1304092560d5dc2e2
dongwoo <imkdw@kakao.com>
2025-09-08 19:39:27 +0900

    레포지토리 의존성주입 에러 수정

--- apps/api/src/shared/repository/repository.module.ts
+++ apps/api/src/shared/repository/repository.module.ts
@@ -2,11 +2,14 @@ import { Global, Module } from '@nestjs/common';
 import { DatabaseModule } from '@/infra/database/database.module';
 import { MemberRepository } from './member/member.repository';
 import { ArticleRepository } from './article/article.repository';
+import { SeriesRepository } from '@/shared/repository/series/series.repository';
+
+const providers = [MemberRepository, ArticleRepository, SeriesRepository];
 
 @Global()
 @Module({
   imports: [DatabaseModule],
-  providers: [MemberRepository, ArticleRepository],
-  exports: [MemberRepository, ArticleRepository],
+  providers,
+  exports: providers,
 })
 export class RepositoryModule {}
