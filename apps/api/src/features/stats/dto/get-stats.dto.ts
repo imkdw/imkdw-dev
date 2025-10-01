788feb4b11aa2db9f35cb4386142a6139111e692
dongwoo <imkdw@kakao.com>
2025-10-01 21:03:10 +0900

    불필요한 생성자 제거

--- apps/api/src/features/stats/dto/get-stats.dto.ts
+++ apps/api/src/features/stats/dto/get-stats.dto.ts
@@ -7,29 +7,16 @@ class ArticleStatsDto implements IArticleStatsDto {
 
   @ApiProperty({ description: '총 게시글 조회수', example: 12450 })
   readonly viewCount: number;
-
-  constructor(count: number, viewCount: number) {
-    this.count = count;
-    this.viewCount = viewCount;
-  }
 }
 
 class SeriesStatsDto implements ISeriesStatsDto {
   @ApiProperty({ description: '총 시리즈 개수', example: 25 })
   readonly count: number;
-
-  constructor(count: number) {
-    this.count = count;
-  }
 }
 
 class TagStatsDto implements ITagStatsDto {
   @ApiProperty({ description: '총 태그 개수', example: 80 })
   readonly count: number;
-
-  constructor(count: number) {
-    this.count = count;
-  }
 }
 
 export class ResponseGetStatsDto implements IResponseGetStatsDto {
@@ -41,10 +28,4 @@ export class ResponseGetStatsDto implements IResponseGetStatsDto {
 
   @ApiProperty({ type: TagStatsDto, description: '태그 통계' })
   readonly tag: TagStatsDto;
-
-  private constructor(article: ArticleStatsDto, series: SeriesStatsDto, tag: TagStatsDto) {
-    this.article = article;
-    this.series = series;
-    this.tag = tag;
-  }
 }
