48a77d942e6d622208b30ac9fd741752e4a95f31
dongwoo <imkdw@kakao.com>
2025-10-07 16:31:47 +0900

    루트 레이아웃에 아톰 원다크 코드블럭 스타일 추가

--- apps/blog/src/app/layout.tsx
+++ apps/blog/src/app/layout.tsx
@@ -4,6 +4,7 @@ import { pretendard } from '@imkdw-dev/fonts';
 import '@mdxeditor/editor/style.css';
 import '@imkdw-dev/ui/globals.css';
 import '@imkdw-dev/ui/milkdown.css';
+import 'highlight.js/styles/atom-one-dark.css';
 
 import { Providers } from '../components/providers';
 import { MobileSidebar } from '../components/sidebar/mobile-sidebar';
