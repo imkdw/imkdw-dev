94bd8c3475b2dc14e5ce4b4c90466b4c6caa8177
dongwoo <imkdw@kakao.com>
2025-10-18 20:53:44 +0900

    글 작성 페이지 접근시 관리자가 아닌 경우 403을 반환하도록 변경

--- apps/blog/src/app/articles/create/page.tsx
+++ apps/blog/src/app/articles/create/page.tsx
@@ -1,7 +1,16 @@
 import { Layout } from '@imkdw-dev/ui';
 import { ArticleForm } from '@/components/article/article-form';
+import { getCurrentMember } from '@imkdw-dev/actions';
+import { MEMBER_ROLE } from '@imkdw-dev/consts';
+import { forbidden } from 'next/navigation';
 
-export default function CreateArticlePage() {
+export default async function CreateArticlePage() {
+  const currentMember = await getCurrentMember();
+
+  if (!currentMember || currentMember.role !== MEMBER_ROLE.ADMIN) {
+    forbidden();
+  }
+
   return (
     <Layout>
       <ArticleForm mode="create" />
