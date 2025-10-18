801df42526ff71b3644e91e4487e6ed8c917c4e4
dongwoo <imkdw@kakao.com>
2025-10-18 20:54:45 +0900

    게시글 수정 페이지 접근시 관리자가 아닌 경우 403을 반환하도록 변경

--- apps/blog/src/app/articles/[slug]/edit/page.tsx
+++ apps/blog/src/app/articles/[slug]/edit/page.tsx
@@ -1,14 +1,21 @@
 import { Layout } from '@imkdw-dev/ui';
 import { ArticleForm } from '@/components/article/article-form';
-import { getArticle } from '@imkdw-dev/actions';
+import { getArticle, getCurrentMember } from '@imkdw-dev/actions';
+import { forbidden } from 'next/navigation';
+import { MEMBER_ROLE } from '@imkdw-dev/consts';
 
 interface Props {
   params: Promise<{ slug: string }>;
 }
 
-export default async function ArticleEditPage({ params }: Props) {
+export default async function EditArticlePage({ params }: Props) {
   const { slug } = await params;
   const { article } = await getArticle(slug);
+  const currentMember = await getCurrentMember();
+
+  if (!currentMember || currentMember.role !== MEMBER_ROLE.ADMIN) {
+    forbidden();
+  }
 
   return (
     <Layout>
