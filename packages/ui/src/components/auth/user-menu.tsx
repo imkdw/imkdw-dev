1027dc98e63f8548fd6d8bb9ff94f6c19a7bc302
dongwoo <imkdw@kakao.com>
2025-10-18 20:53:19 +0900

    관리자인 경우만 글 쓰기 버튼이 노출되도록 변경

--- packages/ui/src/components/auth/user-menu.tsx
+++ packages/ui/src/components/auth/user-menu.tsx
@@ -1,4 +1,4 @@
-import { User, Edit, LogOut } from 'lucide-react';
+import { Edit, LogOut, User } from 'lucide-react';
 import { Button, buttonVariants } from '../../primitives/button';
 import { Avatar, AvatarImage, AvatarFallback } from '../../primitives/avatar';
 import {
@@ -9,6 +9,7 @@ import {
   DropdownMenuTrigger,
 } from '../../primitives/dropdown-menu';
 import { IMemberDto } from '@imkdw-dev/types';
+import { MEMBER_ROLE } from '@imkdw-dev/consts';
 import Link from 'next/link';
 import { cn } from '../../lib/utils';
 
@@ -18,8 +19,8 @@ interface Props {
   onLogout: () => void;
 }
 
-export function UserMenu({ currentMember: user, onLogin, onLogout }: Props) {
-  if (!user) {
+export function currentMemberMenu({ currentMember, onLogin, onLogout }: Props) {
+  if (!currentMember) {
     return <Button onClick={onLogin}>로그인</Button>;
   }
 
@@ -27,32 +28,34 @@ export function UserMenu({ currentMember: user, onLogin, onLogout }: Props) {
     <DropdownMenu>
       <DropdownMenuTrigger className={cn(buttonVariants({ variant: 'ghost' }), 'relative h-8 w-8 rounded-full')}>
         <Avatar className="h-8 w-8">
-          <AvatarImage src={user.profileImage} alt={user.email} />
+          <AvatarImage src={currentMember.profileImage} alt={currentMember.email} />
           <AvatarFallback className="bg-primary text-primary-foreground">
-            {user.email.charAt(0).toUpperCase()}
+            {currentMember.email.charAt(0).toUpperCase()}
           </AvatarFallback>
         </Avatar>
       </DropdownMenuTrigger>
       <DropdownMenuContent className="w-56 border-none" align="end">
         <div className="flex items-center justify-start gap-2 p-2">
           <div className="flex flex-col space-y-1 leading-none">
-            <p className="font-medium">{user.nickname}</p>
-            <p className="w-[200px] truncate text-sm text-muted-foreground">{user.email}</p>
+            <p className="font-medium">{currentMember.nickname}</p>
+            <p className="w-[200px] truncate text-sm text-muted-foreground">{currentMember.email}</p>
           </div>
         </div>
         <DropdownMenuSeparator />
         <DropdownMenuItem>
-          <Link href={`/members/${user.id}`} className="flex items-center justify-center">
+          <Link href={`/members/${currentMember.id}`} className="flex items-center justify-center">
             <User className="mr-2 h-4 w-4" />
             <span>마이페이지</span>
           </Link>
         </DropdownMenuItem>
-        <DropdownMenuItem>
-          <Link href={`/articles/create`} className="flex items-center justify-center">
-            <Edit className="mr-2 h-4 w-4" />
-            <span>글 작성</span>
-          </Link>
-        </DropdownMenuItem>
+        {currentMember.role === MEMBER_ROLE.ADMIN && (
+          <DropdownMenuItem>
+            <Link href={`/articles/create`} className="flex items-center justify-center">
+              <Edit className="mr-2 h-4 w-4" />
+              <span>글 작성</span>
+            </Link>
+          </DropdownMenuItem>
+        )}
         <DropdownMenuSeparator />
         <DropdownMenuItem>
           <LogOut className="mr-2 h-4 w-4" />
