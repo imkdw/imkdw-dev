fc1b726f2b3709c31d4e282955b3e7b5ee4c71d0
김동우(Dongwoo-Kim) <imkdw@kakao.com>
2025-12-18 19:13:12 +0900

    feat: 게시글 상세보기시 상단 읽기 현황에 대한 프로그레스바 추가 (#21)

--- apps/blog/src/hooks/index.ts
+++ apps/blog/src/hooks/index.ts
@@ -1,4 +1,5 @@
 export * from './use-article-form';
 export * from './use-comment-form';
 export * from './use-navigation-progress';
+export * from './use-reading-progress';
 export * from './use-series-form';
